d4ef40472f5bf53bb2ce13a7870f7286baf13ed6
nicorey89 <nicolasthian8907@gmail.com>
2023-09-11 21:52:21 -0300

    solucionando error de exportacion

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
 const {createPool} = require("mysql2/promise");
 const { DB_PORT, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = require("./config.js")
 
-export const pool = createPool({
+const pool = createPool({
     user: DB_USER,
     password: DB_PASSWORD,
     host: DB_HOST,
     port: DB_PORT,
     database: DB_NAME
 })
+
+module.exports = { pool };
