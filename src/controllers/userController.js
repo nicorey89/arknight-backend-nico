bb76c841de6f086685fc9244b698b947a8a4f740
nicorey89 <nicolasthian8907@gmail.com>
2023-09-12 21:11:55 -0300

    haciendo una consulta a la base de datos

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,9 @@
 const {pool} = require("../db.js");
 
 module.exports = {
-    list: (req, res) => {
-        res.send("listado de usuarios")
+    list: async (req, res) => {
+        const [rows] = await pool.query('SELECT * FROM users')
+        res.json(rows)
     },
     detail: (req, res) => {
         res.send("detalle de usuario")
