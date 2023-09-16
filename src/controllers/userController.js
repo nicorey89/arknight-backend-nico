91ef406aebe696730794414adfd76067c91b843a
nicorey89 <nicolasthian8907@gmail.com>
2023-09-15 22:01:38 -0300

    probando create

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,17 +2,17 @@ const {pool} = require("../db.js");
 
 module.exports = {
     list: async (req, res) => {
-        const [rows] = await pool.query('SELECT * FROM users')
+        const [rows] = await pool.query('SELECT * FROM users2')
         res.json(rows)
     },
     detail: async (req, res) => {
         const userID = req.params.id;
-        const [rows] = await pool.query(`SELECT * FROM users WHERE id=${userID}`)
+        const [rows] = await pool.query(`SELECT * FROM users2 WHERE id=${userID}`)
         res.json(rows)
     },   
     create: async (req, res) => {
-        const [rows] = await pool.query(`INSERT INTO users2 (name, email, pass) VALUES ("nicolas", "nico@mail.com", "112233")`)
-        res.json(rows)
+        const result = await pool.query('INSERT INTO users2 (name, email, pass) VALUES ("nicolas", "nico@mail.com", "112233")')
+        res.json(result)
     }, 
 
 }
