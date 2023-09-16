29e03159525d6b6c7c179b634ff45e68841cf65d
nicorey89 <nicolasthian8907@gmail.com>
2023-09-15 21:06:58 -0300

    probando create

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { list, detail} = require("../controllers/productController");
+const { list, detail, create} = require("../controllers/productController");
 
 router
     .get("/", list)
     .get("/:id", detail)
+    .get("/create", create)
 
 module.exports = router;
