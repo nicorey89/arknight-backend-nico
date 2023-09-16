d4548ca15c0ed8f83d41d76d11aa1729ca90e0fa
nicorey89 <nicolasthian8907@gmail.com>
2023-09-15 22:50:38 -0300

    agrego creacion de producto

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -16,23 +16,24 @@ module.exports = {
     productEdit: (req, res) => {
         res.send("hola soy el home")
     },
-    productCreate: (req, res) => {
-        res.send("hola soy el home")
+    productCreate: async (req, res) => {
+        const result = await pool.query('INSERT INTO products (name, brand, price, discount, description, dues, image) VALUES ("amoladora", "de walt", "1111", "10", "csjdcjsdcjdjcjsdbcjbdc", "3", "defaultImage.jpg")')
+        res.json(result)
     },
     productDestroy: (req, res) => {
         res.send("hola soy el home")
     },
     usersList: async (req, res) => {
-        const [rows] = await pool.query('SELECT * FROM users2')
+        const [rows] = await pool.query('SELECT * FROM users')
         res.json(rows)
     },
     userDetail: async (req, res) => {
         const userID = req.params.id;
-        const [rows] = await pool.query(`SELECT * FROM users2 WHERE id=${userID}`)
+        const [rows] = await pool.query(`SELECT * FROM users WHERE id=${userID}`)
         res.json(rows)
     },
     userCreate: async (req, res) => {
-        const result = await pool.query('INSERT INTO users2 (name, email, pass) VALUES ("nicolas", "nico@mail.com", "112233")')
+        const result = await pool.query('INSERT INTO users (name, email, pass) VALUES ("nicolas", "nico@mail.com", "112233")')
         res.json(result)
     },
     userEdit: (req, res) => {
