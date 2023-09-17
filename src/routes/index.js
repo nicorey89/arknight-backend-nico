793a23cb3a8173cf25278af07118d009582078b7
nicorey89 <nicolasthian8907@gmail.com>
2023-09-16 22:34:54 -0300

    probando con postman

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,15 +14,17 @@ const {
     userDestroy} = require("../controllers/indexController");
 
 router.get("/", index);
-router.get("/users", usersList);
-router.get("/products", productsList);
-router.get("/users/:id", userDetail);
+/* router.get("/users", usersList); */
+/* router.get("/products", productsList); */
+router.post("/products", productCreate);
+router.put("/products", productEdit);
+router.delete("/products", productDestroy);
+/* router.get("/users/:id", userDetail);
 router.get("/products/:id", productDetail);
 router.get("/createuser", userCreate);
 router.get("/edituser/:id", userEdit);
 router.get("/editproduct/:id", productEdit);
 router.get("/destroyuser/:id", userDestroy);
-router.get("/destroyproduct/:id", productDestroy);
-router.post("/products", productCreate);
+router.get("/destroyproduct/:id", productDestroy); */
 
 module.exports = router;
